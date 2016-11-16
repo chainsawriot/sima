79ca204f48d65bc55e8b193303f438b7a3e7792f
chainsawriot <chainsawtiney@gmail.com>
2016-11-16 16:22:23 +0800

    Added guess format and YAML support

--- lib/sima.rb
+++ lib/sima.rb
@@ -1,11 +1,36 @@
 require "sima/version"
+require "YAML"
 
 module Sima
-  def self.dump(obj, file_path)
-    File.open(File.expand_path(file_path), 'wb') { |f| f.write(Marshal.dump(obj)) }
+  def self.dump(obj, file_path, format = nil)
+    if format.nil?
+      format = guessformat(file_path)
+    end
+    case format
+    when "mar", "marshal"
+      File.open(File.expand_path(file_path), 'wb') { |f| f.write(Marshal.dump(obj)) }
+    when "yaml", "yml"
+      File.open(File.expand_path(file_path), 'wb') { |f| f.write(YAML.dump(obj)) }
+    else
+      raise ArgumentError, "Unknown serialization format."
+    end
   end
-  def self.load(file_path)
-    obj = Marshal.load(File.read(File.expand_path(file_path)))
+  def self.load(file_path, format = nil)
+    if format.nil?
+      format = guessformat(file_path)
+    end
+    case format
+    when "mar", "marhsal"
+      obj = Marshal.load(File.read(File.expand_path(file_path)))      
+    when "yaml", "yml"
+      obj = YAML.load(File.read(File.expand_path(file_path)))
+    else
+      raise ArgumentError, "Unknown serialization format"
+    end
     return obj
   end
+  def self.guessformat(file_path)
+    return file_path.downcase.split(/\./).last
+  end
+  private_class_method :guessformat
 end
--- lib/sima/version.rb
+++ lib/sima/version.rb
@@ -1,3 +1,3 @@
 module Sima
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
